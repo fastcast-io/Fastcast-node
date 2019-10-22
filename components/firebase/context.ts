022d4057e02f3434b318af59c85b884f2315a1d6
N'Godjigui Junior Diarrassouba <jndiarrassouba@gmail.com>
2019-10-22 10:48:17 -0500

    imported firebase package

--- components/firebase/context.ts
+++ components/firebase/context.ts
@@ -1,6 +1,13 @@
 import React from 'react'
 
+// interface withFirebaseProps {
+//     state: 
+// }
+
 const FirebaseContext = React.createContext(null);
 
 export const FirebaseProvider = FirebaseContext.Provider;
 export const FirebaseConsumer = FirebaseContext.Consumer;
+
+
+// export const withFirebase = <P extends object>(Component: React.ComponentType<P>)
