80b6c2e9dea6cde7a91ce505978cb1c40d84c570
N'Godjigui Junior Diarrassouba <jndiarrassouba@gmail.com>
2019-10-22 16:04:52 -0500

    Added some scopes and custom parameters

--- components/firebase/firebase.ts
+++ components/firebase/firebase.ts
@@ -8,13 +8,31 @@ var app : firebase.app.App = firebase.initializeApp(firebaseConfig)
 
 class Firebase {
     auth: firebase.auth.Auth
-    provider: firebase.auth.AuthProvider
+    provider: firebase.auth.GoogleAuthProvider
 
     constructor() {
         
         this.auth = app.auth()
         this.provider = new firebase.auth.GoogleAuthProvider()
 
+        /**
+         * Adding scopes
+         * as found here:
+         * https://developers.google.com/identity/protocols/googlescopes#scriptv1
+         */
+        this.provider.addScope('https://www.googleapis.com/auth/userinfo.email')
+        this.provider.addScope('https://www.googleapis.com/auth/userinfo.profile')
+        // this.provider.addScope('https://www.googleapis.com/auth/gmail.send')
+        // this.provider.addScope('https://www.googleapis.com/auth/drive')
+        // this.provider.addScope('https://www.googleapis.com/auth/forms')
+        // this.provider.addScope('https://www.googleapis.com/auth/spreadsheets')
+
+        /**
+         * Custom google auth options
+         */
+        this.provider.setCustomParameters({
+            'prompt': 'select_account'
+        })
     }
 
 
