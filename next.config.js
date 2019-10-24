071de57f4a9542e98e3923990f8fe33dd04dfa40
N'Godjigui Junior Diarrassouba <jndiarrassouba@gmail.com>
2019-10-24 18:17:26 -0500

    decided to go with bootstrap

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 const { parsed: localEnv } = require("dotenv").config()
 const webpack = require("webpack")
 const withSass = require("@zeit/next-sass")
+const withCss = require("@zeit/next-css")
 
-module.exports = withSass({
+module.exports = withCss(withSass({
     webpack(config) {
         config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
         return config;
     }
-})
+}))
