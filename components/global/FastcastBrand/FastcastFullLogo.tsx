d0c33533a9d1c4c21ae9258b11fe8ce664c787ac
N'Godjigui Junior Diarrassouba <jndiarrassouba@gmail.com>
2019-10-29 09:30:43 -0500

    updated the fastcast branding references and sizing options

--- components/global/FastcastBrand/FastcastFullLogo.tsx
+++ components/global/FastcastBrand/FastcastFullLogo.tsx
@@ -1,6 +1,14 @@
-const FastcastFullLogo = ({}) => (
+interface imgSizing {
+    styles : {
+        height: string,
+        width: string
+    }
+}
+// TODO FIX DUPLICATE IMGSIZING!!
+
+const FastcastFullLogo = ({ styles }: imgSizing) => (
     <div className="fastcast-brand">
-            <img alt="fastcast-brand" src={'/FastcastFullLogo.svg'} />
+            <img alt="fastcast-brand" src={'/FastcastFullLogo.svg'} style={styles}/>
     </div>
 )
 
--- components/global/FastcastBrand/FastcastLogoOnly.tsx
+++ components/global/FastcastBrand/FastcastLogoOnly.tsx
@@ -1,6 +1,15 @@
-const FastcastLogoOnly = ({}) => (
+interface imgSizing {
+    styles: {
+        height: string,
+        width: string
+    }
+}
+
+// TODO: Fix the duplicate interface for imgSizing !!!!
+
+const FastcastLogoOnly = ({ styles }: imgSizing) => (
     <div className="fastcast-brand">
-            <img alt="fastcast-brand" src={'/FastcastLogoOnly.svg'} />
+            <img alt="fastcast-brand" src={'/FastcastLogoOnly.svg'} style={styles} />
     </div>
 )
 
