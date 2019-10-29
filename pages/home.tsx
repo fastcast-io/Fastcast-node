8bff0465f966e1d178e8e8e7f0933b5b58efdc85
N'Godjigui Junior Diarrassouba <jndiarrassouba@gmail.com>
2019-10-29 13:00:02 -0500

    Tested the action title helper component

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,10 +1,11 @@
 import { Container } from 'react-bootstrap'
+import { ActionTitle } from '../components/Helpers'
 
 // TODO: CREATE A REDIRECT COMPONENT WHICH WRAPS EVERYTHING OR LOADS! AND HAS BACKGROUND
 const Home = (props: React.ComponentProps<any>) => (
     <Container>
         <div>
-            <div>DIS IS HOME</div>
+            <ActionTitle textValue={"Quick Actions"}/>
         </div>
     </Container>
 )
